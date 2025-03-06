e75e28ca9085ac221116d4b46fdd83d8c94cd3d3
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-07 05:45:09 +0700

    feat: add package nodemon, add 2 variant endpoints

--- server.js
+++ server.js
@@ -2,7 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 5758;
 
+// dummy data
+let data = {
+  name: "rizky",
+  job: "job hunter",
+};
+
 // server run in ...
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
+});
+
+// Type 1 - Website endpoints -> these endpoints are for sending back html and they typically come when a user enter a url in a browser
+
+app.get("/", (req, res) => {
+  console.log("Success, Hit this endpoint", req.method);
+  res.send("<h1>Halo Dunia</h1><input />"); // HTML Format
+
+  // res.sendStatus(200).send("Halo Dunia"); // OK
+  // res.sendStatus(201).send("Halo Dunia"); // Created
+});
+
+app.get("/dashboard", (req, res) => {
+  console.log("Hit the endpoint /dashboard");
+  res.send("Halo dashboard");
+});
+
+// Type 2 - API endpoints
+
+app.get("/api/data", (req, res) => {
+  console.log("Endpoint not visual");
+  res.send(data);
 });
