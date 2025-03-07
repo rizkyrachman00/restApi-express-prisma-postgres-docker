2c046dfe7b8c3cf0caca562d1443d3e6fc352690
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-07 07:07:59 +0700

    feat: custom status code response server, delete method endpoint

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = express();
 const PORT = 5758;
 
 // dummy data
-let data = {
-  name: "rizky",
-  job: "job hunter",
-};
+let data = ["Budi-json"];
 
 // Middleware
 app.use(express.json());
@@ -19,13 +16,16 @@ app.listen(PORT, () => {
 // Type 1 - Website endpoints -> these endpoints are for sending back html and they typically come when a user enter a url in a browser
 
 app.get("/", (req, res) => {
-  console.log("Success, Hit this endpoint", req.method);
+  console.log("Success, User requested home page website");
 
   res.send(`
     <body>
       <h1>Data :</h1>
       <p> ${JSON.stringify(data)} </p>
+      <a href="/dashboard">Go to Dashboard</a>
     </body>
+    <script>console.log("Hi from the server")</script>
+
     `);
 
   // res.send("<h1>Halo Dunia</h1><input />"); // HTML Format
@@ -36,18 +36,33 @@ app.get("/", (req, res) => {
 
 app.get("/dashboard", (req, res) => {
   console.log("Hit the endpoint /dashboard");
-  res.send("Halo dashboard");
+  res.send(`
+    <body>
+      <h1>Dashboard</h1>
+      <a href="/">Go To Home</a>
+    </body>
+    `);
 });
 
 // Type 2 - API endpoints
 
 app.get("/api/data", (req, res) => {
   console.log("Endpoint not visual");
-  res.send(data);
+  res.status(200).send(data);
 });
 
 app.post("/api/data", (req, res) => {
   const newEntryData = req.body;
   console.log(newEntryData);
+
+  data.push(newEntryData.name);
+
   res.sendStatus(201);
+});
+
+app.delete("/api/data", (req, res) => {
+  data.pop();
+  console.log("Success delete the element of the end of the array data");
+
+  res.sendStatus(200);
 });
