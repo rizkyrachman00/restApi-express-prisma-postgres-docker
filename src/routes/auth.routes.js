5f248adc1d07bca1b23a12f1e057ae2680432e8c
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-09 10:12:33 +0700

    feat: add login function with return jwt token for authorization crud operation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -40,6 +40,41 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
+    const user = getUser.get(username);
+
+    if (!user) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        message: "Invalid password",
+      });
+    }
+
+    console.log(user);
+
+    // then we have a succesfully authentication
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    res.json({
+      token,
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.sendStatus(503);
+  }
+});
 
 export default router;
