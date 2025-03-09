f576e75e4b0fae13a85b2e68a2b213796747b63a
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-09 09:05:20 +0700

    feat: add register function with encrypt password and create jwt token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -8,9 +8,36 @@ const router = express.Router();
 // Register a new user endpoint /auth/register
 router.post("/register", (req, res) => {
   const { username, password } = req.body;
-  console.log(username, password);
 
-  res.sendStatus(201);
+  // encrypt the password
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  // save the new user and hashed password to the db
+  try {
+    const insertUser = db.prepare(`INSERT INTO users (username,password) 
+      VALUES (?,?)`);
+
+    const result = insertUser.run(username, hashedPassword);
+
+    // now we have a user, add default first todo for the user
+    const defaultTodo = `Hello, add your first todo!`;
+    const insertTodo = db.prepare(`INSERt INTO todos (user_id,task) 
+      VALUES (?,?)`);
+
+    insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+    // create a token
+    const token = jwt.sign(
+      { id: result.lastInsertRowid },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.json({ token });
+  } catch (e) {
+    console.log(e.message);
+    res.sendStatus(503);
+  }
 });
 
 router.post("/login", (req, res) => {});
